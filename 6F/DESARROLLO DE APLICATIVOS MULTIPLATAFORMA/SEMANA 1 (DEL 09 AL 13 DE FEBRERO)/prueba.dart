ca14d5b7ca009de2af7a0b65ce38c4f30255f51b
Faridyrg16 <faridyrg16@gmail.com>
2026-02-18 17:46:39 -0500

    cambios por agregacion de codigo redundante

--- 6F/DESARROLLO DE APLICATIVOS MULTIPLATAFORMA/SEMANA 1 (DEL 09 AL 13 DE FEBRERO)/prueba.dart
+++ 6F/DESARROLLO DE APLICATIVOS MULTIPLATAFORMA/SEMANA 1 (DEL 09 AL 13 DE FEBRERO)/prueba.dart
@@ -42,3 +42,93 @@ void main() {
   print("Código: $codigo");
   print("Código 2: $codigo2");
 }
+
+void mayorDeEdad() {
+  //if else switch for while do-while
+  int edad = 20;
+  if (edad >= 18) {
+    print("Es mayor de edad");
+  } else if (edad < 13) {
+    print("Es menor de edad");
+  }
+}
+
+void Positivo() {
+  int a = 5;
+  if (a > 0) {
+    print('positivo');
+  } else if (a < 0) {
+    print('negativo');
+  } else {
+    print('cero');
+  }
+}
+
+void ParImpar() {
+  int numero = 10;
+  if (numero % 2 == 0) {
+    print('par');
+  } else {
+    print('impar');
+  }
+}
+
+void MayorMenor() {
+  int num1 = 5;
+  int num2 = 10;
+  if (num1 > num2) {
+    print('num1 es mayor que num2');
+  } else if (num1 < num2) {
+    print('num1 es menor que num2');
+  } else {
+    print('num1 y num2 son iguales');
+  }
+}
+
+void SwitchCase() {
+  int dia = 3;
+  switch (dia) {
+    case 1:
+      print('Lunes');
+      break;
+    case 2:
+      print('Martes');
+      break;
+    case 3:
+      print('Miércoles');
+      break;
+    case 4:
+      print('Jueves');
+      break;
+    case 5:
+      print('Viernes');
+      break;
+    case 6:
+      print('Sábado');
+      break;
+    case 7:
+      print('Domingo');
+      break;
+    default:
+      print('Número de día inválido');
+  }
+}
+
+void WhileLoop() {
+  int i = 0;
+  while (i < 5) {
+    print(i);
+    i++;
+  }
+}
+
+void ramdom() {
+  for (int i = 0; i < 3; i++) {
+    print('i = $i');
+  }
+
+  List<String> names = ['Ana', 'Luis', 'Eva'];
+  for (var name in names) {
+    print(name);
+  }
+}
